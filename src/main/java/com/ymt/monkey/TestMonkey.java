aade42d5e5f036fce1559775d8a37a293d785449
ymt <sgsg1986@126.com>
2017-08-17 15:47:36 +0800

    u

--- src/main/java/com/ymt/monkey/TestMonkey.java
+++ src/main/java/com/ymt/monkey/TestMonkey.java
@@ -1,11 +1,15 @@
 package com.ymt.monkey;
 
+import io.appium.java_client.ios.IOSDriver;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.Test;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.*;
+import java.net.URL;
 import java.util.*;
 
 /**
@@ -128,5 +132,49 @@ public class TestMonkey {
         System.out.println("cpu list:"+1%3);
 
 
+
+
     }
+
+    @Test
+    public void testIOS() throws Exception {
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "iOS");
+
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("deviceName", "iPhone (3)");
+        capabilities.setCapability("platformVersion", "9.3.1");
+
+        capabilities.setCapability("bundleId", "com.ymatou.Shopping");
+        capabilities.setCapability("udid", "d36d600b4ece53a8bde8daf869790012a46842c3");
+
+        capabilities.setCapability("automationName", "XCUITest");
+
+        capabilities.setCapability("unicodeKeyboard", "True");
+        capabilities.setCapability("resetKeyboard", "True");
+
+        IOSDriver driver = new IOSDriver (new URL("http://127.0.0.1:4723/wd/hub"),
+                capabilities);
+
+
+        Thread.sleep(5000);
+
+        System.out.println("page source:"+driver.getPageSource());
+
+/*
+           ymt app 返回 控件
+
+           <XCUIElementTypeButton type="XCUIElementTypeButton" name="back" label="back" enabled="true" visible="true" x="5" y="26" width="39" height="30"/>
+           <XCUIElementTypeButton type="XCUIElementTypeButton" name="返回" label="返回" enabled="true" visible="false" x="8" y="31" width="21" height="22"/>
+*/
+
+
+
+
+    }
+
+
 }
